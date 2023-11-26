20dc5ece5eda18dfd94cdbabe76d8153b78b83ef
Rūta <gec.ruta@gmail.com>
2023-11-26 18:00:25 +0200

    Modified Dockerfile. minor adjustments

--- Data/LibDbContext.cs
+++ Data/LibDbContext.cs
@@ -1,25 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using LibraTrack.Data.Entities;
 using LibraTrack.Auth.Model;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace LibraTrack.Data
 {
-	public class LibDbContext : IdentityDbContext<User>
-	{
-		private readonly IConfiguration _configuration;
-		public DbSet<Library> Libraries { get; set; }
-		public DbSet<Section> Sections { get; set; }
-		public DbSet<Book> Books { get; set; }
+    public class LibDbContext : IdentityDbContext<User>
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<Library> Libraries { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Book> Books { get; set; }
 
-		public LibDbContext(IConfiguration configuration)
-		{
-			_configuration = configuration;
-		}
+        public LibDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostGreSQL"));
-		}
-	}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostGreSQL"));
+        }
+    }
 }
